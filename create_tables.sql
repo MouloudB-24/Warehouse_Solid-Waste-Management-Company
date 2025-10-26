1ee9847baa611cd9270fe3dfaef06acb3c0f528a
Mouloud <mouloud.bellil@outlook.fr>
2025-10-26 14:27:02 +0100

    update create tables with data company

--- create_tables.sql
+++ create_tables.sql
@@ -1,37 +1,34 @@
-CREATE TABLE IF NOT EXISTS MyDimDate (
+CREATE TABLE IF NOT EXISTS DimDate (
 	date_id INT PRIMARY KEY,
+	date VARCHAR(12),
 	year INT NOT NULL,
 	quarter INT NOT NULL,
 	quater_name VARCHAR(2) NOT NULL,
 	month INT NOT NULL,
 	month_name VARCHAR(20) NOT NULL,
+	day INT NOT NULL,
 	week_day INT NOT NULL,
-	weekday_name VARCHAR(20) NOT NULL,
-	day INT NOT NULL
+	weekday_name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS DimTruck (
+	truck_id INT PRIMARY KEY,
+	truck_type VARCHAR(255) NOT NULL
 );
 
 
-CREATE TABLE IF NOT EXISTS MyDimWaste (
-	waste_id INT PRIMARY KEY,
-	waste_type VARCHAR(255) NOT NULL
-);
-
-
-CREATE TABLE IF NOT EXISTS MyDimZone (
-	zone_id INT PRIMARY KEY,
-	collection_zone VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS DimStation (
+	station_id INT PRIMARY KEY,
 	city VARCHAR(255) NOT NULL
 );
 
-
-CREATE TABLE IF NOT EXISTS MyFactTrips (
+CREATE TABLE IF NOT EXISTS FactTrips (
 	trip_id INT PRIMARY KEY,
 	date_id INT NOT NULL,
-	Waste_id INT NOT NULL,
-	zone_id INT NOT NULL,
-	waste_collection_in_tons DECIMAL(10, 2) NOT NULL,
-	FOREIGN KEY (date_id) REFERENCES MyDimDate(date_id),
-	FOREIGN KEY (waste_id) REFERENCES MyDimWaste(waste_id),
-	FOREIGN KEY (zone_id) REFERENCES MyDimZone(zone_id)
+	station_id INT NOT NULL,
+	truck_id INT NOT NULL,
+	waste_collected DECIMAL(10, 2) NOT NULL,
+	FOREIGN KEY (date_id) REFERENCES DimDate(date_id),
+	FOREIGN KEY (station_id) REFERENCES DimStation(station_id),
+	FOREIGN KEY (truck_id) REFERENCES DimTruck(truck_id)
 );
-
